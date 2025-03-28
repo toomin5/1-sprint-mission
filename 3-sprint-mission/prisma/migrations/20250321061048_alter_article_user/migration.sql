fea07937332c7f28adb6af855c24d44c7e589339
leeminho <twomean99@gmail.com>
2025-03-28 14:39:26 +0900

    fix

--- 3-sprint-mission/prisma/migrations/20250321061048_alter_article_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Article" ADD COLUMN     "userId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Article" ADD CONSTRAINT "Article_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;

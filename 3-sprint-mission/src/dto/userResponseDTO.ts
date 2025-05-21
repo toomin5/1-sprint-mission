c71d3d14a3c1431d6a3e1a92cb1e5ddad3903f13
leeminho <twomean99@gmail.com>
2025-05-21 13:03:34 +0900

    파일이름변경

--- 3-sprint-mission/src/dto/userResponseDTO.ts
+++
@@ -1,8 +0,0 @@
-import User from '../types/User';
-
-const userResponseDTO = (user: User) => {
-  const { password, ...userWithoutPassword } = user;
-  return userWithoutPassword;
-};
-
-export default userResponseDTO;

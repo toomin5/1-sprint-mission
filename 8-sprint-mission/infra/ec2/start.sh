42af356bb14fcc203fa79d6d5bba773d7fbda074
toomin5 <95535471+toomin5@users.noreply.github.com>
2025-06-09 23:18:41 +0900

    Revert "Part3 sprint11 이민호"

--- 8-sprint-mission/infra/ec2/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-npm run build
-
-pm2 start infra/ec2/ecosystem.config.js
--- 8-sprint-mission/prisma/migrations/20250324073817_alter_likecount/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Article" ADD COLUMN     "likeCount" INTEGER DEFAULT 10;
-
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "likeCount" INTEGER DEFAULT 0;
